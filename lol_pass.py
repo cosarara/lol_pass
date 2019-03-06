211d51c29a0520f20170c0136a9f9796b528bf13
Jaume Delclòs Coll <cosarara97@gmail.com>
2019-03-06 23:53:13 +0100

    mss screenshot

--- lol_pass.py
+++ lol_pass.py
@@ -3,10 +3,11 @@ import cv2
 import numpy as np
 import time
 import subprocess
+from mss import mss
 
 def main():
     subprocess.Popen(["league_nopass"])
-    time.sleep(15) # wait for league to start
+    #time.sleep(15) # wait for league to start
 
     with open('pass.txt') as f:
         pwd = f.readline()
@@ -14,23 +15,24 @@ def main():
     template = cv2.imread('pass.png')
     h, w = template.shape[:2]
 
-    for _ in range(10):
-        screen_rgb = pyautogui.screenshot()
-        screen = cv2.cvtColor(np.array(screen_rgb), cv2.COLOR_RGB2BGR)
-        result = cv2.matchTemplate(screen, template, cv2.TM_CCORR_NORMED)
-        (_, conf, _, loc) = cv2.minMaxLoc(result)
-        print('confidence:', conf)
-        if conf < 0.9:
-            time.sleep(2)
-            continue
-        x, y = loc
-        x += w / 2
-        y += h / 2
-        print(x, y)
-        break
-    else:
-        print('giving up!')
-        return
+    with mss() as sct:
+        for _ in range(10):
+            screen_bgra = np.array(sct.grab(sct.monitors[0]))
+            screen = cv2.cvtColor(screen_bgra, cv2.COLOR_BGRA2BGR)
+            result = cv2.matchTemplate(screen, template, cv2.TM_CCORR_NORMED)
+            (_, conf, _, loc) = cv2.minMaxLoc(result)
+            print('confidence:', conf)
+            if conf < 0.9:
+                time.sleep(2)
+                continue
+            x, y = loc
+            x += w / 2
+            y += h / 2
+            print(x, y)
+            break
+        else:
+            print('giving up!')
+            return
 
     pyautogui.click(x, y)
     pyautogui.typewrite(pwd+'\n', interval=0.05)
